d7e9d03db74ffb110b68db12d5e81a3d08210056
Axel Bresch <abresch@gmx.de>
2022-07-07 18:15:14 +0200

    initial commit

---
+++ Bewaesserung.ino
@@ -0,0 +1,84 @@
+int sensorTopf1Pin = A1;
+int sensorTopf2Pin = A2;
+
+int pumpeTopf1Pin = 12;
+int pumpeTopf2Pin = 13;
+
+//Unterhalb diser Sensorwerte wird nicht gegossen
+int sollTopf1 = 300;
+int sollTopf2 = 300;
+
+int istTopf1 = 700;
+int istTopf2 = 700;
+
+//Wartezeit zwischen den Messungen eine Stunde
+int Wartezeit = 3600000;
+
+//Solang wird bei zu trockenem Boden gepumpt [ms]
+int Pumpzeit = 25000;
+
+void setup() {
+  
+  // Alle Sensoren sind Eingänge
+  pinMode(sensorTopf1Pin, INPUT);
+  pinMode(sensorTopf2Pin, INPUT);
+
+  //Alle Pumpen sind Ausgänge
+  pinMode(pumpeTopf1Pin, OUTPUT);
+  pinMode(pumpeTopf2Pin, OUTPUT);
+
+  //Pumpen zu Begin aus
+  digitalWrite(pumpeTopf1Pin,LOW);
+  digitalWrite(pumpeTopf2Pin,LOW);
+  
+  Serial.begin(9600);
+
+  //Wartezeit für die Senoren zu Beginn
+  delay(2000);
+}
+      
+void loop() {
+
+  //Messen...
+  istTopf1 = analogRead(sensorTopf1Pin);
+  istTopf2 = analogRead(sensorTopf2Pin);
+  
+  //... Werte seriell ausgeben...
+  Serial.print("Feuchtigkeit Topf 1: ");
+  Serial.print(istTopf1);
+  Serial.print("\t");   
+  Serial.print("Feuchtigkeit Topf 2: ");
+  Serial.print(istTopf2);
+  Serial.print("\t");   
+
+  //...und bei Bedarf pumpen
+  if(istTopf1 > sollTopf1) 
+    {
+      digitalWrite(pumpeTopf1Pin, HIGH);
+      Serial.print("Topf1 zu trocken!!!"   );
+      Serial.print("\t");
+      delay(Pumpzeit);
+      digitalWrite(pumpeTopf1Pin, LOW);
+    } 
+  else
+    {
+      Serial.print("Topf1 ist ok!"); 
+      Serial.print("\t");
+    }
+    
+  if(istTopf2 > sollTopf2) 
+    {
+      digitalWrite(pumpeTopf2Pin, HIGH);
+      Serial.print("Topf2 zu trocken!!!"   );
+      Serial.print("\n");
+      delay(Pumpzeit);
+      digitalWrite(pumpeTopf2Pin, LOW);
+    } 
+  else
+    {
+      Serial.print("Topf2 ist ok!"); 
+      Serial.print("\n");
+    }
+   
+delay(Wartezeit);
+}
